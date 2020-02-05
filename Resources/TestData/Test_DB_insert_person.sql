2c5ea2cf5f492439960dd9baa846a4557c0066d5
alexsohn7@gmail.com <alexsohn7@gmail.com>
2020-02-04 20:17:07 -0800

    created an insert person sql file to generate test data

---
+++ Resources/TestData/Test_DB_insert_person.sql
@@ -0,0 +1,3 @@
+INSERT INTO Test_DB.customers(first_name, last_name) VALUES('Jane', 'Smith');
+INSERT INTO Test_DB.customers(first_name, last_name) VALUES('Paul', 'Rang');
+INSERT INTO Test_DB.customers(first_name, last_name) VALUES('Kathy', 'Lo');
